4065287b58741c191d315d11a528de4ee9d8862c
Orji Chiemena <107408948+technicaldepartment48@users.noreply.github.com>
2023-04-24 11:46:44 +0100

    Add files via upload

---
+++ storage/framework/views/511f078cfe9745e4ea54c5144505fc16.php
@@ -0,0 +1,74 @@
+<html lang="en">
+
+<head>
+    
+    <meta name="viewport" content="initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    
+    <style type="text/css">
+
+    </style>
+    
+    </head>
+    
+    <body>
+    
+    <div id="container">
+    
+    <h2>SALES FORM</h2>
+    
+    <div>
+    
+    
+    <form method="post" action="form.php" enctype="multipart/form-data">
+    
+                        <span class="u">Amount:</span>
+                        <input type="text" name="amount"/>
+                        
+                        <span class="p">Day:</span>
+                        <select name="day">
+                            <option>Monday</option>
+                            <option>Tuesday</option>
+                            <option>Wednesday</option>
+                            <option>Thursday</option>
+                            <option>Friday</option>
+                            <option>Saturday</option>
+                            <option>Sunday</option>
+                        </select>
+    
+                        <span class="u">Week:</span>
+                        <input type="text" name="week"/>
+    
+                        <span class="p">Month:</span>
+                        <select name="month">
+                            <option>January</option>
+                            <option>February</option>
+                            <option>March</option>
+                            <option>April</option>
+                            <option>May</option>
+                            <option>June</option>
+                            <option>July</option>
+                            <option>August</option>
+                            <option>September</option>
+                            <option>October</option>
+                            <option>November</option>
+                            <option>December</option>
+                        </select>
+    
+                        <span class="u">Year:</span>
+                        <input type="text" name="year"/>
+    
+                        <input type="submit" value="submit" >
+    
+    </form>
+    
+    </div>
+    
+    </div>
+    
+    </body>
+    
+    </html>
+    
+    
+    
+    <?php /**PATH C:\xampp\htdocs\barber-sales-app\resources\views/index.blade.php ENDPATH**/ ?>
